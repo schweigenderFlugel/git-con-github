448b17b9eb9168931bc037257bb84790de47cf84
schweigenderFlugel <facundoezcastro@gmail.com>
2024-02-14 17:59:22 -0300

    add function to the button

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const show = document.getElementById('show');
 const createText = document.createElement('h1');
 createText.innerHTML = "Esto es una prueba de git con github";
 createText.classList = "text";
+createText.style.display = 'none';
 text.append(createText);
 
 const createButton = document.createElement('button');
 createButton.innerHTML = 'Mostrar';
 createButton.classList = 'button';
 show.append(createButton);
+
+show.addEventListener('click', () => {
+  createText.style.display = 'flex';
+})
